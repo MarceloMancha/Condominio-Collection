3b8a02825f2cde980daf0eef9e98ad1ee9fdb4c3
MarceloMancha <marceloprunonosa@yahoo.com.br>
2026-03-05 17:27:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,12 +10,26 @@ const assets = [
 self.addEventListener('install', event => {
 event.waitUntil(
 caches.open(cacheName).then(cache => {
-cache.addAll(assets);
+return cache.addAll(assets);
+})
+);
+});
+
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(keys
+.filter(key => key !== cacheName)
+.map(key => caches.delete(key))
+);
 })
 );
 });
 
 self.addEventListener('fetch', event => {
+if (event.request.url.indexOf('unpkg.com') > -1) {
+return fetch(event.request);
+}
 event.respondWith(
 caches.match(event.request).then(response => {
 return response || fetch(event.request);
